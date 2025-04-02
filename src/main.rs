419fe26bcc33149da0257d5c4844695ab4f8a2e2
gabrielbwitte <wittegabrielb@gmail.com>
2025-04-02 13:29:30 -0400

    created the functionality to open and read files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query: &String = &args[1];
+    let file_path: &String = &args[2];
+
+    println!("Searching for {query}");
+    print!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Should hava been able to read the file");
+
+    println!("With text:\n{contents}");
 }
